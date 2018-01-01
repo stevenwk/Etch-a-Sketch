5d81db3df8080e2c474287d1d09a670ab183f701
Stevenwk <stevenwkresno@gmail.com>
2018-01-01 14:26:16 +0700

    add random color capability

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const grid=document.querySelector('.grid')
 
+function randomColor(){
+  let R=Math.floor(Math.random()*256);
+  let G=Math.floor(Math.random()*256);
+  let B=Math.floor(Math.random()*256);
+  return `RGB(${R}, ${G}, ${B})`
+}
 
-
-function newSketch(size){
+function newSketch(size,type){
   grid.innerHTML=''
   grid.style.gridTemplate=`repeat(${size},1fr)/repeat(${size},1fr)`;//adjust grid size
   for(i=0;i<size**2;i++){
@@ -11,8 +16,10 @@ function newSketch(size){
     grid.appendChild(box);
   }
   const boxes=document.querySelectorAll('.box')
-  boxes.forEach((box)=>{box.addEventListener('mouseover',(e)=>{e.target.classList.add('boxTouched')})})
+  boxes.forEach((box)=>{box.addEventListener('mouseover',(e)=>{
+    e.target.style.backgroundColor=type=='normal'?'#444':`${randomColor()}`
+  })})
 
 
 }
-newSketch(10)
+newSketch(10,'normal')
