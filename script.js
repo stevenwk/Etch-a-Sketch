ec8ea00323f1e3fa51b0b8cc2dc107d599dcd8e2
Stevenwk <stevenwkresno@gmail.com>
2018-01-01 15:43:49 +0700

    add gradation style and better UI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const grid=document.querySelector('.grid')
+const grid=document.querySelector('.grid');
+const gridSize=document.querySelector('input');
 
 function randomColor(){
   let R=Math.floor(Math.random()*256);
@@ -7,19 +8,25 @@ function randomColor(){
   return `RGB(${R}, ${G}, ${B})`
 }
 
-function newSketch(size,type){
+function newSketch(type){
+  let size=gridSize.value
   grid.innerHTML=''
   grid.style.gridTemplate=`repeat(${size},1fr)/repeat(${size},1fr)`;//adjust grid size
   for(i=0;i<size**2;i++){
     const box=document.createElement('div');
     box.classList.add('box');
+    box.level=239;//initial color level
     grid.appendChild(box);
   }
   const boxes=document.querySelectorAll('.box')
   boxes.forEach((box)=>{box.addEventListener('mouseover',(e)=>{
-    e.target.style.backgroundColor=type=='normal'?'#444':`${randomColor()}`
-  })})
+    if (type=='level'){
+      box.level-=16
+      e.target.style.backgroundColor=`RGB(${box.level},${box.level},${box.level})`
+    }else{
+      e.target.style.backgroundColor=type=='normal'?'#444':`${randomColor()}`
+  }})})
 
 
 }
-newSketch(10,'normal')
+newSketch('normal')
